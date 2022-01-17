676f4740c5eaafd66c699dab4f118b3b79ae928d
JessicaFerrer <96149909+JessicaFerrer@users.noreply.github.com>
2022-01-17 16:38:07 -0600

    First commit

---
+++ CarreritasJS/carreritas.js
@@ -0,0 +1,12 @@
+//Array multidimensional
+var carreritas=[["Lucía","Roberto","María","Jesús","Andrea","José"], ["Cristóbal","Fernanda","Armando"],["Federico"]];
+
+console.log(carreritas);
+
+console.log(`En esta primera parte podemos ver cómo ${carreritas[0][4]} adelanta a ${carreritas[0][2]}, quedando: ${carreritas[0][0]}, ${carreritas[0][1]}, ${carreritas[0][4]}, ${carreritas[0][2]}, ${carreritas[0][3]}, ${carreritas[0][5]}`);
+
+console.log(`Por una falta, ${carreritas[0][5]} ha sido descalificado!!!: ${carreritas[0][0]}, ${carreritas[0][1]}, ${carreritas[0][4]}, ${carreritas[0][2]}, ${carreritas[0][3]}`);
+
+console.log(`Inesperadamente podemos ver la incorporación de 3 nuevos corredores: ${carreritas[0][0]}, ${carreritas[1]}, ${carreritas[0][1]}, ${carreritas[0][4]}, ${carreritas[0][2]}, ${carreritas[0][3]}`);
+
+console.log(`Y para la última vuelta, un nuevo concursante toma el primer puesto quedando los puestos finales de la siguiente manera: ${carreritas[2]}, ${carreritas[0][0]}, ${carreritas[1]}, ${carreritas[0][1]}, ${carreritas[0][4]}, ${carreritas[0][2]}, ${carreritas[0][3]}.`);
